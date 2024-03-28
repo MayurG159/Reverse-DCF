372eb9801c59530c55a35d5f53b62b6d966e9f2c
MayurG159 <gulvemayur2000@gmail.com>
2024-03-28 16:46:07 +0530

    'change'

--- pages/2_📈DCF Valuation.py
+++ pages/2_📈DCF Valuation.py
@@ -26,7 +26,7 @@ def perc_value(a):
 def get_soup(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return bs(response.text, 'html_parser')
+        return bs(response.text, 'lxml')
     else:
         raise Exception(f"Failed to fetch data from {url}")
 
